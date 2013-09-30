7f4eb03390d922a6a143a5b6e04b5cdde41a4143
SchMax <schmax@inbox.ru>
2013-09-30 23:00:07 +0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do
   root  'static_pages#home'
-
-  match '/singup',  to: 'users#new',            via: 'get'        
+  resources :users
+  match '/signup',  to: 'users#new',            via: 'get'        
 
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
